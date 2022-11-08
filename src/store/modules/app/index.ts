7e7872fa38c86f8c22285d5b7ae0bf8afe18a5bb
Scott <1194379729@qq.com>
2022-11-08 18:28:19 +0800

    perf: 🎃delete unused store

--- src/store/modules/app/index.ts
+++ src/store/modules/app/index.ts
@@ -20,17 +20,6 @@ const useAppStore = defineStore('app', {
       // @ts-ignore-next-line
       this.$patch(partial);
     },
-
-    // Change theme color
-    toggleTheme(dark: boolean) {
-      if (dark) {
-        this.theme = 'dark';
-        document.body.setAttribute('arco-theme', 'dark');
-      } else {
-        this.theme = 'light';
-        document.body.removeAttribute('arco-theme');
-      }
-    },
     toggleDevice(device: string) {
       this.device = device;
     },
